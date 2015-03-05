1265fa040de9cd1ec503329610bf2dbee7e293c6
Florent Guillaume <guillaume.florent@live.fr>
2015-03-05 17:29:09 +0100

    delete

--- FitNeo/src/persist/JdbcConstants.java
+++
@@ -1,9 +0,0 @@
-package persist;
-
-public interface JdbcConstants {
-	 static final String DBURL = "jdbc:mysql://fitneo.c9coxdomfgjj.us-west-2.rds.amazonaws.com:3306/FITNEO";
-
-	 static final String DBUSER = "fitneoMaster";
-
-	 static final String DBPASS = "fitneo34";
-}
