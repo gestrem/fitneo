d905bbf578fdc4197bc26b26081d789b239a362e
Florent Guillaume <guillaume.florent@live.fr>
2015-03-20 10:24:44 +0100

    Commit view Arnaud

---
+++ FitNeo/src/ui/ActivityFormView.java
@@ -0,0 +1,92 @@
+package ui;
+
+import javax.swing.JPanel;
+import javax.swing.SpringLayout;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JTable;
+import javax.swing.JList;
+
+public class ActivityFormView extends JPanel {
+	private JTextField tf_ActivityName;
+	private JTextField tf_ShortDesc;
+	private JTextField tf_ActivityManager;
+	private JTextField tf_DetailedDesc;
+
+	/**
+	 * Create the panel.
+	 */
+	public ActivityFormView() {
+		SpringLayout springLayout = new SpringLayout();
+		setLayout(springLayout);
+		
+		JLabel lblActivityName = new JLabel("Activity Name:");
+		springLayout.putConstraint(SpringLayout.NORTH, lblActivityName, 107, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.WEST, lblActivityName, 63, SpringLayout.WEST, this);
+		add(lblActivityName);
+		
+		tf_ActivityName = new JTextField();
+		springLayout.putConstraint(SpringLayout.WEST, tf_ActivityName, 6, SpringLayout.EAST, lblActivityName);
+		springLayout.putConstraint(SpringLayout.EAST, tf_ActivityName, -430, SpringLayout.EAST, this);
+		add(tf_ActivityName);
+		tf_ActivityName.setColumns(10);
+		
+		JLabel lblShortDescription = new JLabel("Short Description:");
+		springLayout.putConstraint(SpringLayout.NORTH, lblShortDescription, 31, SpringLayout.SOUTH, lblActivityName);
+		springLayout.putConstraint(SpringLayout.EAST, lblShortDescription, 0, SpringLayout.EAST, lblActivityName);
+		add(lblShortDescription);
+		
+		tf_ShortDesc = new JTextField();
+		springLayout.putConstraint(SpringLayout.SOUTH, tf_ActivityName, -19, SpringLayout.NORTH, tf_ShortDesc);
+		springLayout.putConstraint(SpringLayout.NORTH, tf_ShortDesc, -6, SpringLayout.NORTH, lblShortDescription);
+		springLayout.putConstraint(SpringLayout.WEST, tf_ShortDesc, 0, SpringLayout.WEST, tf_ActivityName);
+		springLayout.putConstraint(SpringLayout.EAST, tf_ShortDesc, 189, SpringLayout.EAST, lblShortDescription);
+		add(tf_ShortDesc);
+		tf_ShortDesc.setColumns(10);
+		
+		JLabel lblActivityManager = new JLabel("Activity Manager:");
+		springLayout.putConstraint(SpringLayout.NORTH, lblActivityManager, 38, SpringLayout.SOUTH, lblShortDescription);
+		springLayout.putConstraint(SpringLayout.EAST, lblActivityManager, 0, SpringLayout.EAST, lblActivityName);
+		add(lblActivityManager);
+		
+		tf_ActivityManager = new JTextField();
+		springLayout.putConstraint(SpringLayout.NORTH, tf_ActivityManager, -6, SpringLayout.NORTH, lblActivityManager);
+		springLayout.putConstraint(SpringLayout.WEST, tf_ActivityManager, 0, SpringLayout.WEST, tf_ActivityName);
+		springLayout.putConstraint(SpringLayout.EAST, tf_ActivityManager, 0, SpringLayout.EAST, tf_ActivityName);
+		add(tf_ActivityManager);
+		tf_ActivityManager.setColumns(10);
+		
+		JLabel lblDetailedDescription = new JLabel("Detailed Description:");
+		springLayout.putConstraint(SpringLayout.NORTH, lblDetailedDescription, 0, SpringLayout.NORTH, lblActivityName);
+		springLayout.putConstraint(SpringLayout.WEST, lblDetailedDescription, 65, SpringLayout.EAST, tf_ActivityName);
+		add(lblDetailedDescription);
+		
+		tf_DetailedDesc = new JTextField();
+		springLayout.putConstraint(SpringLayout.NORTH, tf_DetailedDesc, 6, SpringLayout.SOUTH, lblDetailedDescription);
+		springLayout.putConstraint(SpringLayout.WEST, tf_DetailedDesc, 65, SpringLayout.EAST, tf_ShortDesc);
+		springLayout.putConstraint(SpringLayout.SOUTH, tf_DetailedDesc, 0, SpringLayout.SOUTH, tf_ActivityManager);
+		springLayout.putConstraint(SpringLayout.EAST, tf_DetailedDesc, -87, SpringLayout.EAST, this);
+		add(tf_DetailedDesc);
+		tf_DetailedDesc.setColumns(10);
+		
+		JButton buttonCancel = new JButton("Cancel");
+		springLayout.putConstraint(SpringLayout.NORTH, buttonCancel, 54, SpringLayout.SOUTH, tf_DetailedDesc);
+		springLayout.putConstraint(SpringLayout.WEST, buttonCancel, 0, SpringLayout.WEST, lblDetailedDescription);
+		springLayout.putConstraint(SpringLayout.EAST, buttonCancel, 0, SpringLayout.EAST, lblDetailedDescription);
+		add(buttonCancel);
+		
+		JButton buttonValid = new JButton("Valid");
+		springLayout.putConstraint(SpringLayout.NORTH, buttonValid, 54, SpringLayout.SOUTH, tf_DetailedDesc);
+		springLayout.putConstraint(SpringLayout.WEST, buttonValid, 6, SpringLayout.EAST, buttonCancel);
+		springLayout.putConstraint(SpringLayout.EAST, buttonValid, -75, SpringLayout.EAST, this);
+		add(buttonValid);
+		
+		JLabel lblNewLabel = new JLabel("Fill activity informations");
+		springLayout.putConstraint(SpringLayout.WEST, lblNewLabel, 272, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.SOUTH, lblNewLabel, -42, SpringLayout.NORTH, tf_ActivityName);
+		springLayout.putConstraint(SpringLayout.EAST, lblNewLabel, -230, SpringLayout.EAST, this);
+		add(lblNewLabel);
+
+	}
+}
---
+++ FitNeo/src/ui/ProfilFormView.java
@@ -0,0 +1,145 @@
+package ui;
+
+import javax.swing.JPanel;
+import javax.swing.SpringLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+
+public class ProfilFormView extends JPanel {
+	private JTextField tf_FirstName;
+	private JTextField tf_LastName;
+	private JTextField tf_adress;
+	private JTextField tf_ZipCode;
+	private JTextField tf_Country;
+	private JTextField tf_Number;
+	private JTextField tf_Email;
+
+	/**
+	 * Create the panel.
+	 */
+	public ProfilFormView() {
+		SpringLayout springLayout = new SpringLayout();
+		setLayout(springLayout);
+		
+		JButton buttonUpdate = new JButton("update profile");
+		springLayout.putConstraint(SpringLayout.WEST, buttonUpdate, 402, SpringLayout.WEST, this);
+		buttonUpdate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		add(buttonUpdate);
+		
+		JButton buttonCancel = new JButton("cancel");
+		springLayout.putConstraint(SpringLayout.NORTH, buttonCancel, 0, SpringLayout.NORTH, buttonUpdate);
+		add(buttonCancel);
+		
+		tf_FirstName = new JTextField();
+		springLayout.putConstraint(SpringLayout.NORTH, tf_FirstName, 61, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.WEST, tf_FirstName, 104, SpringLayout.WEST, this);
+		add(tf_FirstName);
+		tf_FirstName.setColumns(10);
+		
+		JLabel lblFirstName = new JLabel("First Name:");
+		springLayout.putConstraint(SpringLayout.NORTH, lblFirstName, 6, SpringLayout.NORTH, tf_FirstName);
+		springLayout.putConstraint(SpringLayout.WEST, lblFirstName, -84, SpringLayout.WEST, tf_FirstName);
+		springLayout.putConstraint(SpringLayout.EAST, lblFirstName, -12, SpringLayout.WEST, tf_FirstName);
+		add(lblFirstName);
+		
+		JLabel lblLastName = new JLabel("Last Name:");
+		springLayout.putConstraint(SpringLayout.EAST, lblLastName, 0, SpringLayout.EAST, lblFirstName);
+		add(lblLastName);
+		
+		tf_LastName = new JTextField();
+		springLayout.putConstraint(SpringLayout.NORTH, lblLastName, 6, SpringLayout.NORTH, tf_LastName);
+		springLayout.putConstraint(SpringLayout.NORTH, tf_LastName, 9, SpringLayout.SOUTH, tf_FirstName);
+		springLayout.putConstraint(SpringLayout.WEST, tf_LastName, 0, SpringLayout.WEST, tf_FirstName);
+		add(tf_LastName);
+		tf_LastName.setColumns(10);
+		
+		JLabel lblAdress = new JLabel("Adress:");
+		springLayout.putConstraint(SpringLayout.NORTH, lblAdress, 59, SpringLayout.SOUTH, lblLastName);
+		springLayout.putConstraint(SpringLayout.EAST, lblAdress, 0, SpringLayout.EAST, lblFirstName);
+		add(lblAdress);
+		
+		tf_adress = new JTextField();
+		springLayout.putConstraint(SpringLayout.NORTH, tf_adress, 0, SpringLayout.NORTH, lblAdress);
+		springLayout.putConstraint(SpringLayout.WEST, tf_adress, 0, SpringLayout.WEST, tf_FirstName);
+		add(tf_adress);
+		tf_adress.setColumns(10);
+		
+		JLabel lblZipCode = new JLabel("Zip Code:");
+		springLayout.putConstraint(SpringLayout.EAST, lblZipCode, 0, SpringLayout.EAST, lblFirstName);
+		add(lblZipCode);
+		
+		tf_ZipCode = new JTextField();
+		springLayout.putConstraint(SpringLayout.SOUTH, tf_adress, -18, SpringLayout.NORTH, tf_ZipCode);
+		springLayout.putConstraint(SpringLayout.NORTH, lblZipCode, 6, SpringLayout.NORTH, tf_ZipCode);
+		springLayout.putConstraint(SpringLayout.WEST, tf_ZipCode, 0, SpringLayout.WEST, tf_FirstName);
+		add(tf_ZipCode);
+		tf_ZipCode.setColumns(10);
+		
+		JLabel lblCountry = new JLabel("Country:");
+		springLayout.putConstraint(SpringLayout.SOUTH, lblCountry, -71, SpringLayout.SOUTH, this);
+		springLayout.putConstraint(SpringLayout.EAST, lblCountry, 0, SpringLayout.EAST, lblFirstName);
+		add(lblCountry);
+		
+		tf_Country = new JTextField();
+		springLayout.putConstraint(SpringLayout.SOUTH, tf_ZipCode, -18, SpringLayout.NORTH, tf_Country);
+		springLayout.putConstraint(SpringLayout.NORTH, tf_Country, -6, SpringLayout.NORTH, lblCountry);
+		springLayout.putConstraint(SpringLayout.EAST, tf_Country, 0, SpringLayout.EAST, tf_FirstName);
+		add(tf_Country);
+		tf_Country.setColumns(10);
+		
+		JLabel lblNumber = new JLabel("Number:");
+		springLayout.putConstraint(SpringLayout.NORTH, lblNumber, 6, SpringLayout.NORTH, tf_FirstName);
+		springLayout.putConstraint(SpringLayout.WEST, lblNumber, 29, SpringLayout.EAST, tf_FirstName);
+		add(lblNumber);
+		
+		tf_Number = new JTextField();
+		springLayout.putConstraint(SpringLayout.WEST, tf_Number, 6, SpringLayout.EAST, lblNumber);
+		add(tf_Number);
+		tf_Number.setColumns(10);
+		
+		tf_Email = new JTextField();
+		springLayout.putConstraint(SpringLayout.NORTH, tf_Email, 98, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.SOUTH, tf_Number, -9, SpringLayout.NORTH, tf_Email);
+		springLayout.putConstraint(SpringLayout.EAST, tf_Number, 0, SpringLayout.EAST, tf_Email);
+		add(tf_Email);
+		tf_Email.setColumns(10);
+		
+		JLabel lblEmail = new JLabel("Email:");
+		springLayout.putConstraint(SpringLayout.WEST, tf_Email, 6, SpringLayout.EAST, lblEmail);
+		springLayout.putConstraint(SpringLayout.EAST, tf_Email, 166, SpringLayout.EAST, lblEmail);
+		springLayout.putConstraint(SpringLayout.SOUTH, lblEmail, 0, SpringLayout.SOUTH, lblLastName);
+		springLayout.putConstraint(SpringLayout.EAST, lblEmail, 0, SpringLayout.EAST, lblNumber);
+		add(lblEmail);
+		
+		JButton buttonChangePwd = new JButton("Change your password");
+		springLayout.putConstraint(SpringLayout.SOUTH, buttonChangePwd, -98, SpringLayout.SOUTH, this);
+		springLayout.putConstraint(SpringLayout.EAST, buttonChangePwd, -240, SpringLayout.EAST, this);
+		springLayout.putConstraint(SpringLayout.NORTH, buttonUpdate, 6, SpringLayout.SOUTH, buttonChangePwd);
+		springLayout.putConstraint(SpringLayout.WEST, buttonCancel, 0, SpringLayout.WEST, buttonChangePwd);
+		springLayout.putConstraint(SpringLayout.WEST, buttonChangePwd, 77, SpringLayout.EAST, tf_adress);
+		buttonChangePwd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		add(buttonChangePwd);
+		
+		JLabel lblNewLabel = new JLabel("To update your profile,");
+		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel, 10, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.WEST, lblNewLabel, 172, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.EAST, lblNewLabel, 0, SpringLayout.EAST, lblNumber);
+		add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("fill fields you need");
+		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel_1, 2, SpringLayout.SOUTH, lblNewLabel);
+		springLayout.putConstraint(SpringLayout.WEST, lblNewLabel_1, 181, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.EAST, lblNewLabel_1, 0, SpringLayout.EAST, lblNumber);
+		add(lblNewLabel_1);
+
+	}
+}
