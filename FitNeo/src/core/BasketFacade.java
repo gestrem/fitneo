55ea5fb0d204d4879c8ef0b5bebc98cda07fb29b
Florent Guillaume <guillaume.florent@live.fr>
2015-03-29 01:10:00 +0100

    Commit Basket2

--- FitNeo/src/core/BasketFacade.java
+++ FitNeo/src/core/BasketFacade.java
@@ -1,10 +1,36 @@
 package core;
 
+import java.util.ArrayList;
+
 public class BasketFacade {
-	private Basket basket;
+	private ListBasket basket;
 	
 	public BasketFacade(int persistType){
 		//On crée une room du type de persistance choisi
-		basket = Basket.getInstance(persistType);
+		basket = ListBasket.getInstance(persistType);
 	}
+	
+	public void loadMainBasket(int idUser){
+		basket.loadMainBasket(idUser);
+	}
+	
+	public void loadOrders(int idUser){
+		basket.loadOrders(idUser);
+	}
+	
+	public void confirmOrder(int idUser){
+		basket.confirmOrder(idUser);
+	}
+	
+	public Basket getMainBasket(){
+		return basket.getMainBasket();
+	}
+	
+	public ArrayList<Basket> getOrders() {
+		return basket.getOrders();
+	}
+/*
+	public int getBasketPrice() {
+		return basket.getBasketPrice();
+	}*/
 }
